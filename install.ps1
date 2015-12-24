78dee5d5d2e07dcbc7a9403f3d90b56e724cfbfb
Florian Hassanen <florian.hassanen@gmail.com>
2015-12-24 13:26:20 +0100

    disable job, instead of rescheduling it
    for some reason my machine does not regard
    the year for wake timers. although the timer
    was set for the year 2022, the computer woke
    two days ago.

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 $Name = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $Principal = New-ScheduledTaskPrincipal -UserId $Name -LogonType S4U
 $Time = New-ScheduledTaskTrigger -At "00:00" -Once -RepetitionInterval "00:05:00"
-$Action = New-ScheduledTaskAction -Execute PowerShell -Argument '-NoLogo -NonInteractive -Command "Set-ScheduledTask Reboot \Microsoft\Windows\UpdateOrchestrator\ -Trigger $(New-ScheduledTaskTrigger -At 2022-2-22-22:22 -Once)"'
+$Action = New-ScheduledTaskAction -Execute PowerShell -Argument '-NoLogo -NonInteractive -Command "Disable-ScheduledTask Reboot \Microsoft\Windows\UpdateOrchestrator\ "'
 Register-ScheduledTask -TaskName "FixPeskyWindowsUpdateReboot" -Principal $Principal -Trigger $Time -Action $Action
