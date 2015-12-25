76fd35e30b5de8f9f101bcc06a0bb299c7bfb0e5
Florian Hassanen <florian.hassanen@gmail.com>
2015-12-25 23:55:04 +0100

    provide hint to run as admin if unsuccessful

--- install.ps1
+++ install.ps1
@@ -2,4 +2,11 @@ $Name = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $Principal = New-ScheduledTaskPrincipal -UserId $Name -LogonType S4U
 $Time = New-ScheduledTaskTrigger -At "00:00" -Once -RepetitionInterval "00:05:00"
 $Action = New-ScheduledTaskAction -Execute PowerShell -Argument '-NoLogo -NonInteractive -Command "Disable-ScheduledTask Reboot \Microsoft\Windows\UpdateOrchestrator\ "'
-Register-ScheduledTask -TaskName "FixPeskyWindowsUpdateReboot" -Principal $Principal -Trigger $Time -Action $Action
+try {
+    Register-ScheduledTask -TaskName "FixPeskyWindowsUpdateReboot" -Principal $Principal -Trigger $Time -Action $Action -ErrorAction Stop
+} catch {
+    write-host "Exception Type: $($_.Exception.Message)" -ForegroundColor RED
+    If ($_.Exception.Message -match "Access is denied") {
+        write-host "You may try running the script with Administrator privileges"
+    }
+}
--- uninstall.ps1
+++ uninstall.ps1
@@ -1,1 +1,8 @@
-Unregister-ScheduledTask -TaskName "FixPeskyWindowsUpdateReboot" -Confirm:$false
+try {
+    Unregister-ScheduledTask -TaskName "FixPeskyWindowsUpdateReboot" -Confirm:$false -ErrorAction Stop
+} catch {
+    write-host "Exception Type: $($_.Exception.Message)" -ForegroundColor RED
+    If ($_.Exception.Message -match "Access is denied") {
+        write-host "You may try running the script with Administrator privileges"
+    }
+}
