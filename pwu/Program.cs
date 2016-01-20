16620a23243ffd9cf952a9d7dfd3efe873a756b3
Florian Hassanen <florian.hassanen@gmail.com>
2016-01-20 15:31:13 +0100

    provide .net program to do the job

---
+++ pwu/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace pwu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SystemEvents.PowerModeChanged += SystemEvents_PowerModeChanged;
+            Thread.Sleep(Timeout.InfiniteTimeSpan);
+        }
+
+        private static void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
+        {
+            if (e.Mode != PowerModes.Suspend)
+            {
+                return;
+            }
+
+            TaskScheduler.TaskScheduler t = new TaskScheduler.TaskScheduler();
+            t.Connect();
+
+            t.GetFolder("\\Microsoft\\Windows\\UpdateOrchestrator").GetTask("Reboot").Enabled = false;
+        }
+    }
+}
